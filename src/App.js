1672fa94713b9f88f4e0087c59686f6e26e563d6
salina225 <[skhanom@madisoncollege.edu]>
2021-02-09 18:29:16 -0600

    Showing my first react component
    App is the root component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Greeting from './Greetings';
 
 const App = () => {
   return (
     <>
-    <h1>
-    Hello React!!
-    </h1>
-    <h2>
-    Hello Karly!!!
-    </h2>
-    <h2>
-    Hello Mike!!!
-    </h2>
+      <h1>
+        Hello React!!
+      </h1>
+      <h2>
+        Hello Karly!!!
+      </h2>
+      <h2>
+        Hello Mike!!!
+      </h2>
+      <Greeting />
+      <Greeting />
+      <Greeting />
+      <Greeting />
     </>
   );
 }
