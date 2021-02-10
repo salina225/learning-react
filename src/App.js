2d0bd472c090bb51248ba0e81b012908162c7b11
salina225 <[skhanom@madisoncollege.edu]>
2021-02-09 18:21:20 -0600

    Learn React Fragment is/was???

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import './App.css';
 
 const App = () => {
   return (
+    <>
     <h1>
     Hello React!!
     </h1>
+    <h2>
+    Hello Karly!!!
+    </h2>
+    <h2>
+    Hello Mike!!!
+    </h2>
+    </>
   );
 }
 
